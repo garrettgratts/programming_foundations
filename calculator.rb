77ef4b7e17e887845ef1012ab26413b7b01381ac
Garrett Gratts <garrettgratts21@gmail.com>
2022-03-28 19:15:45 -0500

    update

--- calculator.rb
+++ calculator.rb
@@ -18,7 +18,7 @@ def name_validator
 end
 
 def num_validator(num)
-  num.to_i.to_s == num
+  (num.to_i.to_s == num) || (num.to_f.to_s == num)
 end
 
 def num_retriever
@@ -26,15 +26,51 @@ def num_retriever
     user_input = gets.chomp
 
     if num_validator(user_input)
-      user_input
+      return user_input
     else
       prompt('Please enter valid number')
     end
   end
 end
 
+def valid_operator(num)
+  %w(1 2 3 4).include?(num)
+end
+
+def get_operator
+  loop do
+  operator = gets.chomp
+
+    if valid_operator(operator)
+      return operator
+    else
+      prompt('Please enter valid number')
+    end
+  end
+end
+
+def float?(num)
+  num.to_f.to_s == num
+end
+
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def calculate(operator, num1, num2)
+  if 
+  
+  case operator
+  when '1'
+  when '2'
+  when '3'
+  when '4'
+end
+
 prompt('Hello, welcome to calculator! Enter your name:')
 name = name_validator
+
+prompt("Hello, #{name}!")
 
 prompt('Enter first number:')
 first_number = num_retriever
@@ -42,5 +78,7 @@ first_number = num_retriever
 prompt('Enter second number:')
 second_number = num_retriever
 
-p first_number
-p second_number
+prompt('Select method of operation: 1) add 2) subtract 3) multiply 4) divide')
+operator = get_operator
+
+calculate(operator, first_number, second_number)
