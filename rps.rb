121e9abb64cfd0728f3161f734130b6f1219ecf3
Garrett Gratts <garrettgratts21@gmail.com>
2022-04-07 01:20:34 -0500

    completed

--- rps.rb
+++ rps.rb
@@ -1,57 +1,45 @@
 CHOICES = %w(rock paper scissors lizard spock)
 
+WIN_LOGIC = {
+  rock: %w(lizard scissors),
+  paper: %w(rock spock),
+  scissors: %w(paper lizard),
+  lizard: %w(spock paper),
+  spock: %w(scissors rock)
+}
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-def win_logic (user, computer)
-  user == 'scissors' && computer == 'paper' ||
-    user == 'paper' && computer == 'rock' ||
-    user == 'rock' && computer == 'lizard' ||
-    user == 'lizard' && computer == 'spock' ||
-    user == 'spock' && computer == 'scissors' ||
-    user == 'scissors' && computer == 'lizard' ||
-    user == 'lizard' && computer == 'paper' ||
-    user == 'paper' && computer == 'spock' ||
-    user == 'spock' && computer == 'rock' ||
-    user == 'rock' && computer == 'scissors'
+def valid_choice(input)
+  CHOICES.include?(input)
 end
 
-def get_choice
-  loop do
-    input = gets.chomp
-
-    return input if CHOICES.include?(input)
-    prompt('Hmm... that does not look like a valid answer.')
+def translate(input)
+  case input
+  when 'r'  then input << 'ock'
+  when 'p'  then input << 'aper'
+  when 'sc' then input << 'issors'
+  when 'l'  then input << 'izard'
+  when 'sp' then input << 'ock'
   end
 end
 
-def valid_choice(input)
-  input == 'r' ||
-    input == 'p' ||
-    input == 'sc' ||
-    input == 'l' ||
-    input == 'sp' ||
-    input == 'rock' ||
-    input == 'paper' ||
-    input == 'scissors' ||
-    input == 'lizard' ||
-    input == 'spock'
-end
-
 def get_choice
   loop do
-    input = gets.chomp
-  
+    input = gets.chomp.downcase
+
+    translate(input)
     return input if valid_choice(input)
     prompt('Hmm... that does not look like valid choice')
   end
 end
 
 def compare_choice(user, computer)
-  if win_logic(user, computer)
+  if WIN_LOGIC[user.to_sym].include?(computer)
     'win'
-  elsif win_logic(computer, user)
+  elsif WIN_LOGIC[computer.to_sym].include?(user)
     'lose'
   else
     'tie'
@@ -61,11 +49,24 @@ end
 def display_result(result)
   if result == 'win'
     prompt('You win!')
-  elsif
-    result == 'lose'
+  elsif result == 'lose'
     prompt('You lost!')
   else
     prompt('It is a tie!')
+  end
+end
+
+def game_complete?(player_score, computer_score)
+  player_score == 3 || computer_score == 3
+end
+
+def display_grand_winner(player_score, computer_score)
+  if player_score == 3
+    prompt('### You are the grand winner! ###')
+    prompt('---------------------------------')
+  elsif computer_score == 3
+    prompt('### HAL is the grand winner! ###')
+    prompt('--------------------------------')
   end
 end
 
@@ -73,41 +74,41 @@ def get_answer
   loop do
     input = gets.chomp.downcase
 
-    if input.start_with?('y') || input.start_with?('n')
-      return input
-    else
-      prompt('Hmm... that does not look like a valid answer')
-    end
+    return input if input.start_with?('y') || input.start_with?('n')
+    prompt('Hmm... that does not look like a valid answer')
   end
 end
 
-prompt('Welcome to rock paper scissors lizard spock!')
-prompt('This is a best of 5!')
+prompt('Welcome to Rock Paper Scissors Lizard Spock!')
+prompt('Compete in a best of 5 versus HAL 9000!')
 
-user_score = 0
-comp_score = 0
+player_score = 0
+computer_score = 0
 
 loop do
   prompt('Make your choice:')
-  choice = get_choice
+  player_choice = get_choice
   computer_choice = CHOICES.sample
 
-  prompt("You chose #{choice}; computer chose #{computer_choice}")
+  prompt("You chose #{player_choice}; HAL chose #{computer_choice}")
 
-  result = compare_choice(choice, computer_choice)
+  result = compare_choice(player_choice, computer_choice)
   display_result(result)
 
-  if result == 'win'
-    user_score += 1
-  elsif result == 'lose'
-    comp_score += 1
-  else result == 'tie'
-    (user_score += 0) && (comp_score += 0)
-  end
+  player_score   += 1 if result == 'win'
+  computer_score += 1 if result == 'lose'
 
-  p user_score
-  p comp_score
-  break if user_score == 3 || comp_score == 3
+  prompt("#{player_score} vs #{computer_score}")
+
+  if game_complete?(player_score, computer_score)
+    display_grand_winner(player_score, computer_score)
+
+    prompt('Play again? (y/n)')
+    break if get_answer == 'n'
+
+    player_score = 0
+    computer_score = 0
+  end
 end
 
-prompt('Thank you for playing rock paper scissors lizard spock!')
+prompt('Thank you for playing Rock Paper Scissors Lizard Spock!')
